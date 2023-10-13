7aaaedba5c6a220f1711aefd666c43d19346fa9b
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-10-13 17:58:19 +1100

    Added add event ts component

--- FIT2095/src/app/add-event/add-event.component.ts
+++ FIT2095/src/app/add-event/add-event.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from '../database.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-event',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-event.component.css']
 })
 export class AddEventComponent {
+  name: string = '';
+  description: string = '';
+  image: string = '';
+  date: Date = new Date();
+  duration: number = 0;
+  isActive: boolean = false;
+  capacity: number = 0;
+  availability: number = 0;
+  categoryId: string = '';
+
+  constructor(public dbService: DatabaseService, private router:Router) { }
+
+  saveEvent(){
+    let categoryObj = {
+      name: this.name,
+      description: this.description,
+      image: this.image,
+      date: this.date,
+      duration: this.duration,
+      isActive: this.isActive,
+      capacity: this.capacity,
+      availability: this.availability,
+      categoryId: this.categoryId
+    }
+
+    this.dbService.createEvent(categoryObj).subscribe({
+      next:(result) => {this.router.navigate(['/list-event']);},
+      error: (error) => {console.log(error);}
+    })
+  }
 
 }
